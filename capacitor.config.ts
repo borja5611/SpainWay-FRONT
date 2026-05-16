f638b99a67f0cb8246a77326219fedbacceec812
borja5611 <123094981+borja5611@users.noreply.github.com>
2026-05-17 01:03:32 +0200

    Añade configuración Android Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.spainway.app',
+  appName: 'SpainWay',
+  webDir: 'dist'
+};
+
+export default config;
